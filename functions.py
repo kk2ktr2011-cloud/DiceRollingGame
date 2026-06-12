a34a74e32f9f823a82c34a2c539697b2a6c33996
kk2ktr2011-cloud <kk2ktr2011@gmail.com>
2026-06-12 11:02:26 -0500

    Added All Visual Aspects

--- functions.py
+++ functions.py
@@ -1,4 +1,4 @@
-import random
+import random, time
 
 def roll_dice_for_num_of_rolls(target):
     num_to_get = target
@@ -6,12 +6,17 @@ def roll_dice_for_num_of_rolls(target):
     num_of_rolls = 0
     while num_of_rolls < 20:
         if roll < num_to_get:
-            roll += random.randint(1, 6)
+            num_rolled = random.randint(1, 6)
+            roll += num_rolled
             num_of_rolls += 1
+            display_number(num_rolled)
         elif roll > num_to_get:
-            roll -= random.randint(1, 6)
+            num_rolled = random.randint(1, 6)
+            roll -= num_rolled
             num_of_rolls += 1
+            display_number(num_rolled)
         elif roll == num_to_get:
+            print("That was the last die for this player!")
             break
     return num_of_rolls
 
@@ -38,3 +43,44 @@ def display_number(number):
                |   O   |
                |_______|
                             """)
+        time.sleep(1)
+    elif number == 2:
+        print("""
+                _______
+               | O     |
+               |       |
+               |_____O_|
+                            """)
+        time.sleep(1)
+    elif number == 3:
+        print("""
+                _______
+               | O     |
+               |   O   |
+               |_____O_|
+                            """)
+        time.sleep(1)
+    elif number == 4:
+        print("""
+                _______
+               | O   O |
+               |       |
+               | O___O |
+                            """)
+        time.sleep(3)
+    elif number == 5:
+        print("""
+                _______
+               | O   O |
+               |   O   |
+               | O___O |
+                            """)
+        time.sleep(1)    
+    elif number == 6:
+        print("""
+                _______
+               | O   O |
+               | O   O |
+               | O___O |
+                            """)
+        time.sleep(1)
